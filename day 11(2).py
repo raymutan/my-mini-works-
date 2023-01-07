ce3782a5dff36308c33d6f9ce94a00c6a4d1bbd6
raymutan <122161147+raymutan@users.noreply.github.com>
2023-01-07 21:35:46 +0800

    Add files via upload

---
+++ day 11(2).py
@@ -0,0 +1,52 @@
+num1=int(input('pls put number'))
+num2=int(input('pls put number2'))
+def option():
+ mdas=(int(input('pls choose to multiply(1),divide(2),add(3),subtract(4)')))
+ if(mdas==1):
+     print(num1*num2)
+ elif(mdas==2):
+     print(num1/num2)
+ elif(mdas==3):
+     print(num1+num2)
+ elif(mdas==4):
+     print(num1-num2)
+option()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
+
+
+
+
+
+
+
+
+
