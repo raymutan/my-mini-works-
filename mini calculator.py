a0c0f2cd357f626c2a226990f85588f1574f359e
raymutan <122161147+raymutan@users.noreply.github.com>
2023-01-09 19:20:34 +0800

    Add files via upload

---
+++ mini calculator.py
@@ -0,0 +1,55 @@
+
+
+
+num1=(float(input('pls put number')))
+num2=(float(input('pls put number2')))
+def option():
+ mdas=(int(input('pls choose to multiply(1),divide(2),add(3),subtract(4)')))
+ if(mdas==1):
+     print('{} '.format(num1)+ 'multipied by {} '.format(num2)+'is {}'.format(num1*num2))
+ elif(mdas==2):
+     print('{} '.format(num1)+ 'divided by {} '.format(num2)+'is {}'.format(num1/num2))
+ elif(mdas==3):
+     print('{} '.format(num1)+ 'added with {} '.format(num2)+'is {}'.format(num1+num2))
+ elif(mdas==4):
+     print('{} '.format(num1)+ 'subtracted by {} '.format(num2)+'is {}'.format(num1-num2))
+option()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
+
+
+
+
+
+
+
+
+
---
+++ username and password.py
@@ -0,0 +1,37 @@
+
+from getpass import getpass
+print('sign up today')
+name1 = (input('your username will be '))
+pass1 =(getpass('your password is '))
+userpass=(input('username:'))
+if (userpass!= name1):
+    print('invalid username')
+    quit()
+else:
+    passpass = (getpass('password: '))
+if (passpass!=pass1):
+    print('invalid password')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
