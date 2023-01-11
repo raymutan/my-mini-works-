36c689c5e6e920f8122fab618042e816c9504aab
raymutan <122161147+raymutan@users.noreply.github.com>
2023-01-11 10:35:16 +0800

    Update passchecker(incomplete).py

--- passchecker(incomplete).py
+++ passchecker(incomplete).py
@@ -1,23 +1,17 @@
 import re 
-
+flag=1
+def invalid():
+  print('Password must contain characters from two of the following four categories: Uppercase characters A-Z  Lowercase characters a-z Digits 0-9. Special characters (!, $, #, %, etc.)')
 def passcheck(password):
     if (len(password)<8):
-        print('Password must contain characters from two of the following four categories: Uppercase characters A-Z  Lowercase characters a-z  Digits 0-9. Special characters (!, $, #, %, etc.)')
+      invalid() 
     elif not re.search("[a-z]", password):
-      print('Password must contain characters from two of the following four categories: Uppercase characters A-Z  Lowercase characters a-z  Digits 0-9. Special characters (!, $, #, %, etc.) ')
+     invalid()
     elif not re.search("[A-Z]", password):
-      print('Password must contain characters from two of the following four categories: Uppercase characters A-Z  Lowercase characters a-z  Digits 0-9. Special characters (!, $, #, %, etc.)')
-    elif not re.search("[0-9]", password):
-      print('Password must contain characters from two of the following four categories: Uppercase characters A-Z  Lowercase characters a-z  Digits 0-9. Special characters (!, $, #, %, etc.)')
-    elif not re.search("[!@$%&*]" , password):
-       print('Password must contain characters from two of the following four categories: Uppercase characters A-Z  Lowercase characters a-z Digits 0-9. Special characters (!, $, #, %, etc.)')
-       
+      invalid()  
     else:
       print('password successfully created!')
-
-
 passcheck(input('password is'))
-
 
 
 
