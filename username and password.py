fff0d97baec6fd51223a2fba084452f5c44e7c1e
raymutan <122161147+raymutan@users.noreply.github.com>
2023-01-11 14:42:10 +0800

    Update username and password.py

--- username and password.py
+++ username and password.py
@@ -1,41 +1,47 @@
 import re
 import colorama
 from colorama import Fore
+
+
 def invalid():
- print(Fore.RED  + 'Password must contain characters from two of the following four categories: Uppercase characters A-Z  Lowercase characters a-z Digits 0-9. Special characters (!, $, #, %, etc.)')
+  print(
+    Fore.RED +
+    'Password must contain characters from two of the following four categories: Uppercase characters A-Z  Lowercase characters a-z Digits 0-9. Special characters (!, $, #, %, etc.)'
+  )
+  quit()
 
- 
+
 def passcheck(password):
-    if (len(password)<8):
-     invalid()
-    elif not re.search("[a-z]", password):
-     invalid()
-    elif not re.search("[A-Z]", password):
-      invalid()
-    elif not re.search("[0-9]", password):
-      invalid()
-    elif not re.search("[!$%#]" , password):
-      invalid()
-    else:
-      print('password successfully created!')
-      
+  if (len(password) < 8):
+    invalid()
+  elif not re.search("[a-z]", password):
+    invalid()
+  elif not re.search("[A-Z]", password):
+    invalid()
+  elif not re.search("[0-9]", password):
+    invalid()
+  elif not re.search("[!$%#]", password):
+    invalid()
+  else:
+    print('password successfully created!')
 
- #emulate terminal as outer console
+#emulate terminal as outer console
 from getpass import getpass
+
 print('sign up today')
 
 name1 = (input('your username will be '))
-pass1 =(input('your password is '))
+pass1 = (input('your password is '))
 passcheck(pass1)
 
-userpass=(input('username:'))
-if (userpass!= name1):
-    print('wrong username')
-    quit()
+userpass = (input('username:'))
+if (userpass != name1):
+  print('wrong username')
+  quit()
 else:
-    passpass = (getpass('password: '))
-if (passpass!=pass1):
-    print('wrong password')
+  passpass = (getpass('password: '))
+if (passpass != pass1):
+  print('wrong password')
 
 
 
