badcf6682006e9ea8c4d526601a76387650c054d
raymutan <122161147+raymutan@users.noreply.github.com>
2023-01-11 11:17:29 +0800

    Update username and password.py

--- username and password.py
+++ username and password.py
@@ -1,16 +1,45 @@
-#emulate terminal as outer console
+import re
+import colorama
+from colorama import Fore
+def invalid():
+ print(Fore.RED  + 'Password must contain characters from two of the following four categories: Uppercase characters A-Z  Lowercase characters a-z Digits 0-9. Special characters (!, $, #, %, etc.)')
+
+ 
+def passcheck(password):
+    if (len(password)<8):
+     invalid()
+    elif not re.search("[a-z]", password):
+     invalid()
+    elif not re.search("[A-Z]", password):
+      invalid()
+    elif not re.search("[0-9]", password):
+      invalid()
+    elif not re.search("[!$%#]" , password):
+      invalid()
+    else:
+      print('password successfully created!')
+      
+
+ #emulate terminal as outer console
 from getpass import getpass
 print('sign up today')
+
 name1 = (input('your username will be '))
-pass1 =(getpass('your password is '))
+pass1 =(input('your password is '))
+passcheck(pass1)
+
 userpass=(input('username:'))
 if (userpass!= name1):
-    print('invalid username')
+    print('wrong username')
     quit()
 else:
     passpass = (getpass('password: '))
 if (passpass!=pass1):
-    print('invalid password')
+    print('wrong password')
+
+
+
+                  
 
 
 
