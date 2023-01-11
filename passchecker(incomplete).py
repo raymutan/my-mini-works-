b2928e4f7b477b8e692309e9ec0ed3b355a8aff3
raymutan <122161147+raymutan@users.noreply.github.com>
2023-01-11 10:47:56 +0800

    Update passchecker(incomplete).py

--- passchecker(incomplete).py
+++ passchecker(incomplete).py
@@ -1,16 +1,22 @@
-import re 
-flag=1
+import re
+import colorama
+from colorama import Fore
 def invalid():
-  print('Password must contain characters from two of the following four categories: Uppercase characters A-Z  Lowercase characters a-z Digits 0-9. Special characters (!, $, #, %, etc.)')
+ print(Fore.RED  + 'Password must contain characters from two of the following four categories: Uppercase characters A-Z  Lowercase characters a-z Digits 0-9. Special characters (!, $, #, %, etc.)')
 def passcheck(password):
     if (len(password)<8):
       invalid() 
     elif not re.search("[a-z]", password):
      invalid()
     elif not re.search("[A-Z]", password):
-      invalid()  
+      invalid()
+    elif not re.search("[0-9]", password):
+      invalid()
+    elif not re.search("[_@$]" , password):
+      invalid()
     else:
       print('password successfully created!')
+      
 passcheck(input('password is'))
 
 
